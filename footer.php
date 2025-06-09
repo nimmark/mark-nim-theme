83f9aa5caa3f1a5ab55e6ae95aaef860698387ba
Mark Nim <nimmark98@gmail.com>
2025-06-09 17:49:40 +0800

    make date dynamic

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer class="site-footer">
     <div class="site-footer__container">
         <h5 class="heading-5 site-logo"><a href="<?= site_url() ?>" class="site-logo__url--white">M.Nim</a></h5>
-        <h6 class="heading-6 text-white">&#xa9; 2025 Mark G. Nim. All rights reserved</h6>
+        <h6 class="heading-6 text-white">&#xa9; <?= date('Y') ?> Mark G. Nim. All rights reserved</h6>
     </div>
 
 
